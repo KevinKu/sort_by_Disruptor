a3daf621c0d1250a1f6a3e8054a9d137d1a5ba1a
KF <koftwro@gmail.com>
2016-08-01 22:07:14 +0800

    Finish: 	[test_main.c]:add test_create_multi_thread 	[test_tool.py]:add test_get_empty_job_env_init To do: 	multi-thread get_empty_job test

--- test_main.c
+++ test_main.c
@@ -1,3 +1,22 @@
+/*
+*Test define use:
+*
+*	TEST_THREAD_COMMIT_JOB:
+*
+*		test Job_Disruptor.commit_job
+*
+*
+*	
+*
+*
+*
+*
+*/
+
+
+#define TEST_THREAD_COMMIT_JOB
+
+
 #include "Disruptor.h"
 #include <pthread.h>
 #define waiting 0
@@ -7,6 +26,8 @@
 int test_max_num;
 
 int start = 1;
+
+int test_thread_num;
 
 pthread_t test_thread_a,test_thread_b;
 
@@ -20,7 +41,7 @@ void *test_multi_thread_commit_job(void *arg)
 {
 
 
-//while(start == 1){__sync_synchronize();}
+while(start == 1){__sync_synchronize();}
 
 
 Job *job_array = (Job *)arg;
@@ -40,12 +61,55 @@ return (void*)NULL ;
 }
 
 
+void *test_get_empty_job(void* arg)
+{
+while(start == 1){__sync_synchronize();}
+
+Job_Disruptor.get_empty_job();
+
+return (void*)NULL;
+
+}
+
+
+void test_create_multi_thread(void *(*test_function)(void *),void *arg,const int thread_num)
+{
+int i = 0;
+pthread_t thread_id;
+
+	for(;i < thread_num;i++)
+	{
+	
+	pthread_create(&thread_id,0,test_function,arg);
+
+	}
+
+return;
+
+}
+
+
 int main(int argc,char *argv[])
 {
+
+#ifdef TEST_THREAD_COMMIT_JOB
 
 pthread_create(&test_thread_a,0,test_multi_thread_commit_job,&test_job_list_a);
 
 pthread_create(&test_thread_b,0,test_multi_thread_commit_job,&test_job_list_b);
+
+#endif
+
+#ifdef TEST_THREAD_GET_EMPTY_JOB
+
+test_create_multi_thread(test_get_empty_job,0,test_thread_num);
+
+
+#endif
+
+
+#ifdef
+
 
 return 0;
 }
--- test_tool.py
+++ test_tool.py
@@ -1,6 +1,21 @@
 import gdb
 import string
 
+class test_get_empty_job_env_init(gdb.Command):
+
+	def __init__(self):
+		super(test_get_empty_job_env_init,self).__init__("test_get_empty_job_env_init",gdb.COMMAND_USER)
+
+	def invoke(self,arg,from_tty):		
+
+		c = "p Job_Disruptor.Create_Job_list("+ arg +",1)"
+		gdb.execute(c,True,True)
+
+		c = "p Job_Disruptor.Register_Reader(1,0,0)"
+		gdb.execute(c,True,True)
+	
+		print("init success")
+
 class insert_job_list_from_file(gdb.Command):
 
 	def __init__(self):
@@ -23,7 +38,8 @@ class insert_job_list_from_file(gdb.Command):
 				c="p "+job_list_head+"["+str(i)+"]"+".worker="+"'"+data[3]+"'"
 				gdb.execute(c,False,False)
 				i = i + 1
-
+	
 
 
 insert_job_list_from_file()
+test_get_empty_job_env_init()
